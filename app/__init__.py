91cb207841ef7b11d20be0e462b4596f5286ceee
James Allen <james.m.allen@gmail.com>
2019-04-28 00:34:57 -0400

    Add talisman to force https

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_talisman import Talisman
+
+
 from .characters import CHARACTERS
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
+csp = {
+    'default-src': '*'
+}
+Talisman(app, content_security_policy=csp)
 
 from . import routes
